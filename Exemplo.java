50c8209892f50ae968f0174a25c539e0edbca62e
marcoweb <marcocarvalho.web@gmail.com>
2023-08-15 19:32:18 +0000

    Exemplo de Aula

---
+++ Exemplo.java
@@ -0,0 +1,8 @@
+public class Exemplo {
+    public static void main(String[] args) {
+        Aluno al = new Aluno();
+        al.setNome("Marco");
+
+        System.out.println(al.getNome());
+    }
+}
